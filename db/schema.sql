5fd718f0d51c4af4c21a2ce0816edd62c36614a3
A Suicidal Cow <aaron.bertagnole@gmail.com>
2020-06-14 19:15:21 -0600

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,9 @@
 DROP DATABASE IF EXISTS registeredUsersdb;
 
 CREATE DATABASE registeredUsersdb;
+use registeredUsersdb;
+CREATE TABLE users (
+username VARCHAR(45),
+ password VARCHAR(15)
+ );
+ SELECT * FROM users ;
